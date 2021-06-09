d6f545693645fffde2ef5acac9bc7250343ee461
Curtis Kapsak <kapsakcj@gmail.com>
2021-06-09 23:58:43 +0000

    added function for checking existence of files. checks for dry run and actual concatenation work properly now.

--- concatenate-across-lanes.sh
+++ concatenate-across-lanes.sh
@@ -8,6 +8,10 @@
 
 # totally stolen and modified from https://www.biostars.org/p/317385/
 
+# function to check if there are any matches (one or more)
+# https://stackoverflow.com/questions/24615535/bash-check-if-file-exists-with-double-bracket-test-and-wildcards
+exists() { [[ -f $1 ]]; }
+
 # first argument must be target directory
 TARGET_DIR=$1
 echo "TARGET_DIR is set to $TARGET_DIR"
@@ -26,11 +30,11 @@ if [[ "${DRY_RUN}" == "dry" ]]; then
   echo "DRY RUN OPTION SPECIFIED, NOT ACTUALLY CONCATENATING HERE..."
   # cut -c 22n is because this has 21 characters in it: _L002_R2_001.fastq.gz 
   # STARTING FROM 22ND CHARACTER, PRINT TIL THE END. THIS IS THE VARIABLE F WHICH IS THE SAMPLE ID
-  for i in $(find ${1} -maxdepth 1 -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq); do
+  for i in $(find ${TARGET_DIR} -maxdepth 1 -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq); do
       echo "Merging R1 for ${i}"
       echo "cat "${i}"_L00*_R1_001.fastq.gz > "${i}"_merged_R1.fastq.gz"
     # check to see if R2 exists before concatenating & making an empty file when there is no R2
-    if [[ -e ${i}*R2*.fastq.gz ]]; then
+    if exists ${TARGET_DIR}/${i}*R2*.fastq.gz ; then
       echo "Merging R2 for ${i}"
       echo "cat "${i}"_L00*_R2_001.fastq.gz > "${i}"_merged_R2.fastq.gz"
     else
@@ -43,16 +47,16 @@ fi
 
 # actual concatenate
 # maxdepth 1 so it only looks for fastq.gz's in the supplied path
-for i in $(find $1 -maxdepth 1 -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq); do
- echo "Merging R1 for ${i}"
- cat "${i}"_L00*_R1_001.fastq.gz > "${i}"_merged_R1.fastq.gz
- # check to see if R2 exists before concatenating & making an empty file when there is no R2
- if [[ -e ${i}*R2*.fastq.gz ]]; then
-   echo "Merging R2 for ${i}"
-   cat "${i}"_L00*_R2_001.fastq.gz > "${i}"_merged_R2.fastq.gz
- else
-   echo "R2 file for ${i} was not found. Skipping R2 concatenation..."
- fi
+for i in $(find ${TARGET_DIR} -maxdepth 1 -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq); do
+  echo "Merging R1 for ${i}"
+  cat "${i}"_L00*_R1_001.fastq.gz > "${i}"_merged_R1.fastq.gz
+  # check to see if R2 exists before concatenating & making an empty file when there is no R2
+  if exists ${TARGET_DIR}/${i}*R2*.fastq.gz ; then
+    echo "Merging R2 for ${i}"
+    cat "${i}"_L00*_R2_001.fastq.gz > "${i}"_merged_R2.fastq.gz
+  else
+    echo "R2 file for ${i} was not found. Skipping R2 concatenation..."
+  fi
 done
 
 date
